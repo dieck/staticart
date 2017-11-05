38ad749f0fb1aa1e0128bfa8b29b6e75c799e264
Marco Dieckhoff <marco.dieckhoff@googlemail.com>
2017-11-06 00:09:56 +0100

    added drewnoakes/metadata-extractor as subtree

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # staticart
 Static Web Gallery exporter with subdirectory, EXIF and XMP support
+
+
---
+++ src/name/dieckhoff/staticart/StaticArt.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package name.dieckhoff.staticart;
+
+/**
+ * @author dieck
+ *
+ */
+public class StaticArt {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {		
+		
+
+	}
+
+}
